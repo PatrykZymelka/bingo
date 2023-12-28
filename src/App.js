ef3d91858cdb4ae7a7b18773cfdb7a54af645624
PatrykZymelka <patrykzymelka@op.pl>
2023-12-28 22:51:24 +0100

    Created: Project Base

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Background">
+      <div className="Top"></div>
+      <div className="Middle">
+        <div className="SideBar"></div>
+        <div className="Content">
+          <div className="Bingo"></div>
+        </div>
+        <div className="SideBar"></div>
+      </div>
+      <div className="Bottom"></div>
+    </div>
+  );
+}
+
+export default App;
